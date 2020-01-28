a877e7c3e3d7f4475a936f78d2da4e67fa38e62e
Roger McConkie <roger.mcconkiejr@gmail.com>
2020-01-27 17:20:01 -0700

    99% of queues and stacks done

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -116,29 +116,35 @@ class DoublyLinkedList:
     the node was the head or the tail"""
     def delete(self, node):
         self.length -= 1
-
-        if self.head is self.tail:
+        # If LL is empty
+        if not self.head and not self.tail:
+            # TODO: Error handling
+            self.length = 0
+            return
+        # If head and tail
+        if self.head == self.tail:
             self.head = None
             self.tail = None
-        elif node is self.head:
-            self.head = node.next
+        # if head
+        elif self.head == node:
+            self.head = self.head.next
             node.delete()
-        elif node is self.tail:
-            self.tail = node.prev
+        # if tail
+        elif self.tail == node:
+            self.tail = self.tail.prev
             node.delete()
+        # otherwise
         else:
-            node.delete
+            node.delete()
         
     """Returns the highest value currently in the list"""
     def get_max(self):
-        if not self.head:
+        if self.head is None:
             return None
-        
-        max_val = self.head.value
+        max_value = self.head.value
         current = self.head
-
         while current:
-            if current.value > max_val:
-                max_val = current.value
+            if current.value > max_value:
+                max_value = current.value
             current = current.next
-        return max_val
+        return max_value
